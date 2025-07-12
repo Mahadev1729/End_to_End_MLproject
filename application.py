36415f9c289e780626cd32602c3d5797bf1f3efe
Mahadev1729 <devcampus17@gmail.com>
2025-07-12 11:10:48 +0530

    Add application.py for Flask web application setup and update requirements.txt for Flask capitalization

---
+++ application.py
@@ -0,0 +1,20 @@
+﻿from flask import Flask,request,jsonify,render_template
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+app=application
+
+## import ridge and scaler
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+scaler_model=pickle.load(open('models/scaler.pkl','rb'))
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello<h>"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
